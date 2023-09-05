3ae0626ee33bfdb74db70d6f10cdd077f619add4
jianbiao.zhou <jianbiao.zhou@mxplayer.in>
2023-09-05 17:34:30 +0800

    test2

--- app/src/main/java/com/example/nativecdemo/MainActivity.kt
+++ app/src/main/java/com/example/nativecdemo/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.nativecdemo.databinding.ActivityMainBinding
 /**
  * CMakeLists.txt
  *  test modified branch1 branch 2
+ *  add something in branch2
  */
 class MainActivity : AppCompatActivity() {
 
